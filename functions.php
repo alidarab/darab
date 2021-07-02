d54793079b4277ed1d836735ee51a27e1e34ffb7
Ali Darab <alidarab@live.com>
2021-07-02 09:23:04 -0400

    add nav menu

--- functions.php
+++ functions.php
@@ -22,4 +22,17 @@ if( function_exists('acf_add_options_page') ) {
 
 }
 
+// Register Navigation
+
+function register_cms_menu() {
+  register_nav_menus(
+    array(
+      'header-menu' => __( 'Header Menu' ),
+      'footer-menu' => __( 'Footer Menu' )
+     )
+   );
+ }
+ add_action( 'init', 'register_cms_menu' );
+
 ?>
+
